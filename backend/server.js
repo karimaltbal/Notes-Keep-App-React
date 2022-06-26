e8bba5ef01f6d48c11d7b2bf183c77d97306bd23
karimaltbal <altbalkarim@gmail.com>
2022-06-26 11:32:25 +0200

    make it better

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,6 @@ const userRouter = require("./routes/userRoutes");
 const noteRouter = require("./routes/notesRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
-const path = require("path");
 const app = express();
 
 
@@ -20,21 +19,6 @@ app.use(express.json())
 app.use("/api/users", userRouter);
 app.use("/api/notes", noteRouter);
 
-// --------------------------deployment------------------------------
- __dirname = path.resolve();
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
-// --------------------------deployment------------------------------
 
 app.use(notFound);
 app.use(errorHandler);
@@ -47,7 +31,7 @@ app.get("/", (req, res)=>{
     res.send("wolcame")
 })
 
-
+/*
 app.get("/api/notes", (req, res) => {
     res.json(notes);
 });
@@ -56,7 +40,7 @@ app.get("/api/notes/:id", (req, res) => {
     const note = notes.find((n)=> n._id == req.params.id);
     res.json(note)
 });
-
+*/
 
 const PORT = process.env.PORT || 5000
 
