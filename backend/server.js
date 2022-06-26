212f3b7eb9707565289f63596039e5970fae1ff9
karimaltbal <altbalkarim@gmail.com>
2022-06-26 11:06:00 +0200

    make it betteru

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use("/api/users", userRouter);
 app.use("/api/notes", noteRouter);
 
 // --------------------------deployment------------------------------
-var __dirname = path.resolve();
+ __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
