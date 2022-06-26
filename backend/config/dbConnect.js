d5b39b14a5ea88f0d980afbfd05bb20d4906ff71
karimaltbal <altbalkarim@gmail.com>
2022-06-26 06:22:55 +0200

    make it better

--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const dbCoonect = ()=>{
 
-    mongoose.connect("mongodb+srv://don:nk6T3UWKCc1lU6MH@cluster0.imcic.mongodb.net/Notes-Keeping",{
+    mongoose.connect(process.env.MONGO_URI,{
         useNewUrlParser: true,
         useUnifiedTopology: true
     }).then(()=>{
