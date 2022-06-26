4e5b4f9bac8cc0bcce11841e39fe18a9166e43fa
karimaltbal <altbalkarim@gmail.com>
2022-06-26 12:35:15 +0200

    make it better

--- backend/server.js
+++ backend/server.js
@@ -25,17 +25,6 @@ app.use(errorHandler);
 
 
 
-/*
-app.get("/api/notes", (req, res) => {
-    res.json(notes);
-});
-
-app.get("/api/notes/:id", (req, res) => {
-    const note = notes.find((n)=> n._id == req.params.id);
-    res.json(note)
-});
-*/
-
 const PORT = process.env.PORT || 5000
 
 app.get("/", (req, res) => {
